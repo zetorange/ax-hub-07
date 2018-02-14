afc8f03469ce2a619186a859b46809f08b5ab570
blackboat <zygroup1@gmail.com>
2018-02-14 06:49:40 +0400

    update

--- hub_linux.py
+++ hub_linux.py
@@ -72,12 +72,13 @@ def act_mouse():
 def act_keyboard():
     time.sleep(1)
     subprocess.call(['wmctrl', '-a', 'gedit'])
-    N = random.randint(50, 100)
+    N = random.randint(30, 60)
     random_str = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(N))
-    speed = float(random.randint(2,3))/10
+    speed = float(random.randint(8,10))/10
     for s in random_str:
-        if 'gedit' not in get_active_window():
-            return
+        if get_active_window():
+            if 'gedit' not in get_active_window():
+                return
         pyautogui.typewrite(s)
         time.sleep(speed)
 
